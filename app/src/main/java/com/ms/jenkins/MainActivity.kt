7d7a971281eba4931da0838bd6fe623a75851617
Mavya Soni <onlysoni3@gmail.com>
2025-05-17 20:03:27 +0530

    Update the label

--- app/src/main/java/com/ms/jenkins/MainActivity.kt
+++ app/src/main/java/com/ms/jenkins/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             JenkinsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Jenkins Demo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
